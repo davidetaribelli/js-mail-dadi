a103c0253143c6d29aff5ae4e47b1cf5ceacf3fa
davidetaribelli <davide.taribelli@gmail.com>
2023-04-18 17:31:47 +0200

    aggiunto variabile booleana per terminare ciclo

--- js/main.js
+++ js/main.js
@@ -2,20 +2,31 @@
 // l'email inserita è nella lista?
 // se l'email è nella lista allora l'utente potrà accedere 
 // se invece l'email è "sconosciuta" l'utente non potrà accedere
-
-let emailValida = ["davide.taribelli@gmail.com","luca.lambiase@gmail.com", "mario.rossi99@gmail.com" ];
-
 let login = prompt("Scrivimi la tua email");
+let emailValida = ["davide.taribelli@gmail.com","luca.lambiase@gmail.com", "mario.rossi99@gmail.com" ];
+let accesso = false; //variabile booleana
+
 
 for (let i = 0; i < emailValida.length ; i++) {
 
     const emailUtente = emailValida[i];
-    // controllo se quello che è stato inserito è uguale a uno dei valori di emailUtente
-    if(login == emailUtente){
-        alert(`Sei riuscito ad accedere`);
-    } else if (login != emailUtente){
-        alert(`Non sei riuscito ad accedere`);
+    
+    if(login == emailUtente){  //se l'email è corretta la variabile diventa true terminando il ciclo 
+        accesso = true;
     }
 }
+
+if (accesso == true) {  // se è true allora riesci ad accedere, altrimenti no
+
+    alert(`Sei riuscito ad accedere`);
+
+}else{
+    alert(`Non sei riuscito ad accedere`);
+}
+
+
+
+
+    
 
 
